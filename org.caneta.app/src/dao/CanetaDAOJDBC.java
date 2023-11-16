e0a7c2565873a617b43faf4b850ae8e3fed46221
Kauã Lima <59632185+eukaualima@users.noreply.github.com>
2023-11-16 15:15:32 -0300

    :sparkles: dao CanetaDAOJDBC

---
+++ org.caneta.app/src/dao/CanetaDAOJDBC.java
@@ -0,0 +1,33 @@
+package dao;
+
+import java.util.List;
+import modelo.Caneta;
+
+public class CanetaDAOJDBC implements CanetaDAO
+{
+    @Override
+    public int inserir(Caneta contato) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int editar(Caneta contato) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int apagar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Caneta> listar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Caneta listar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
