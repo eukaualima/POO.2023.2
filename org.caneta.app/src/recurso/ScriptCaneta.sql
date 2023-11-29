41a32782546618fbc384a6860341c24cc62918c3
Kauã Lima <59632185+eukaualima@users.noreply.github.com>
2023-11-29 16:50:05 -0300

    :zap: mudança no sql

--- org.caneta.app/src/recurso/ScriptCaneta.sql
+++ org.caneta.app/src/recurso/ScriptCaneta.sql
@@ -2,15 +2,19 @@ CREATE DATABASE IF NOT EXISTS papelaria;
 
 USE papelaria;
 
-CREATE TABLE canetas (codigo INT AUTO_INCREMENT PRIMARY KEY, modelo VARCHAR(50), cor VARCHAR(50), ponta DECIMAL(2,1), carga INT, tampada BOOLEAN);
+CREATE TABLE modelos (codigo INT AUTO_INCREMENT PRIMARY KEY, modelo VARCHAR(255));
+CREATE TABLE canetas (codigo INT AUTO_INCREMENT PRIMARY KEY, codmod INT, cor VARCHAR(50), ponta DECIMAL(2,1), carga INT, tampada BOOLEAN, FOREIGN KEY (modelo) REFERENCES codmod(codigo));
+    
+INSERT INTO modelos (modelo) VALUES ('Bic');
+INSERT INTO modelos (modelo) VALUES ('Pilot');
+INSERT INTO modelos (modelo) VALUES ('Faber-Castell');
+INSERT INTO modelos (modelo) VALUES ('Stabilo');
+INSERT INTO modelos (modelo) VALUES ('Sharpie');
+INSERT INTO modelos (modelo) VALUES ('Paper Mate');
 
-INSERT INTO canetas (modelo, cor, ponta, carga, tampada) VALUES ('Bic', 'Verde', 1.0, 100, true);
-INSERT INTO canetas (modelo, cor, ponta, carga, tampada) VALUES ('Pilot', 'Azul', 0.7, 50, false);
-INSERT INTO canetas (modelo, cor, ponta, carga, tampada) VALUES ('Faber-Castell', 'Preto', 0.5, 80, true);
-INSERT INTO canetas (modelo, cor, ponta, carga, tampada) VALUES ('Stabilo', 'Rosa', 1.2, 20, false);
-INSERT INTO canetas (modelo, cor, ponta, carga, tampada) VALUES ('Sharpie', 'Amarelo', 0.8, 60, true);
-INSERT INTO canetas (modelo, cor, ponta, carga, tampada) VALUES ('Paper Mate', 'Laranja', 1.4, 40, false);
-INSERT INTO canetas (modelo, cor, ponta, carga, tampada) VALUES ('Uniball', 'Vermelho', 0.5, 70, true);
-INSERT INTO canetas (modelo, cor, ponta, carga, tampada) VALUES ('Pentel', 'Azul', 1.0, 90, false);
-INSERT INTO canetas (modelo, cor, ponta, carga, tampada) VALUES ('Zebra', 'Verde', 0.7, 30, true);
-INSERT INTO canetas (modelo, cor, ponta, carga, tampada) VALUES ('Copic', 'Preto', 0.5, 50, false);
+INSERT INTO canetas (modelo, cor, ponta, carga, tampada) VALUES (0, 'Verde', 1.0, 100, true);
+INSERT INTO canetas (modelo, cor, ponta, carga, tampada) VALUES (1, 'Azul', 0.7, 50, false);
+INSERT INTO canetas (modelo, cor, ponta, carga, tampada) VALUES (2, 'Preto', 0.5, 80, true);
+INSERT INTO canetas (modelo, cor, ponta, carga, tampada) VALUES (3, 'Rosa', 1.2, 20, false);
+INSERT INTO canetas (modelo, cor, ponta, carga, tampada) VALUES (4, 'Amarelo', 0.8, 60, true);
+INSERT INTO canetas (modelo, cor, ponta, carga, tampada) VALUES (5, 'Laranja', 1.4, 40, false);
